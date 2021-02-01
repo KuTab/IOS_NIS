ec37b731f96e9077edda3bd8e822f5b4c4ae4e41
KuTab <58887990+KuTab@users.noreply.github.com>
2021-02-01 23:37:02 +0300

    Add files via upload

---
+++ FancyButtons/FancyButtons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FancyButtons
+//
+//  Created by Egor Dadugin on 01.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
